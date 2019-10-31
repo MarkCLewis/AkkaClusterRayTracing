07d0b9bd757e3c5a28c25b1f4b17d64dff97a45e
Kurt Hardee <khardee@trinity.edu>
2019-10-31 16:08:02 -0500

    fixed hierarchy

--- src/main/scala/mud/ImageDrawer.scala
+++ src/main/scala/mud/ImageDrawer.scala
@@ -11,22 +11,39 @@ import scala.concurrent.duration._
 import akka.util.Timeout
 import akka.actor.ActorRef
 import playground.RTBufferedImage
+import Combiners._
 
-class ImageDrawer(geom: Geometry, lights: List[Light], img: rendersim.RTBufferedImage, numRays: Int) extends Actor{
+class ImageDrawer(geom: Geometry, lights: List[Light], img: rendersim.RTBufferedImage, numRays: Int) extends Actor {
   import ImageDrawer._
 
   val aspect = img.width.toDouble / img.height
   implicit val timeout = Timeout(100.seconds)
   implicit val ec = context.dispatcher
   val manager:ActorRef = context.actorOf(Props(new RTManager(geom, lights)), "RTMan")
+  var ids: Map[Long, IntersectData => Boolean] = Nil
+  val mc:IDCombiner = new MergeColors(1)
+  val ml:IDCombiner = new MergeLightSource(1)
+
   def receive = {
     case Start(eye, topLeft, right, down) =>  
       for (i <- (0 until img.width); j <- (0 until img.height)) {
-        (0 until numRays).map(index => {
+        /*(0 until numRays).map(index => {
             manager ! RTManager.CastRay(i, j, 
               Ray(eye, topLeft + right * (aspect * (i + (if (index > 0) math.random * 0.75 else 0)) / img.width) + down * (j + (if (index > 0) math.random * 0.75 else 0)) / img.height)
             )}
-            )
+            )*/
+          val r = Ray(eye, topLeft + right * (aspect * (i + (if (index > 0) math.random * 0.75 else 0)) / img.width) + down * (j + (if (index > 0) math.random * 0.75 else 0)) / img.height)
+          val intsct = geom intersect r
+          intsct match {
+            case None => self ! setColor(i,j,RTColor(0.0, 0.0, 0.0, 1.0))
+            case Some(intD) => {
+              val geomSize = intD.geom.boundingSphere.radius
+              val id = scala.util.Random.nextLong()
+              ids = ids ++ (id, ml.merge())
+            }
+          }
+
+
       }
     case SetColor(i, j, color) =>
             println(s"Setting $i, $j")
--- src/main/scala/mud/RTActor.scala
+++ src/main/scala/mud/RTActor.scala
@@ -15,6 +15,22 @@ class RTActor(geom: Geometry, lights: List[Light]) extends Actor {
     }
     case _ =>
   }
+  def intersectGeom(r: Ray, s: ActorRef, id: Long) = {
+    //TODO: Use a blockingQueue
+    if(cnt > 5) new RTColor(0, 0, 0, 1)
+    else {
+      val oIntD = geom intersect r
+      oIntD match {
+        case None =>
+        case Some(intD) => {
+          val geomSize = intD.geom.boundingSphere.radius
+          //probably going to be done in the merge light source class
+          val lightColors = for (light <- lights) yield light.color(intD, geom)
+        }
+      }
+    }
+  }
+
 }
 object RTActor {
   case class CastRay(i: Int, j: Int, ray: Ray, drawer: ActorRef)
