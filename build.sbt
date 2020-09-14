ea5f7f91edff5977d5c0d2daf167d198c78a40cc
Kurt Hardee <khardee@trinity.edu>
2020-09-14 14:38:10 -0500

    Moved to Pandora

--- build.sbt
+++ build.sbt
@@ -6,15 +6,9 @@ scalaVersion := "2.12.10"
 //javaHome := Some(file("D:\\graalvm-ce-19.2.1\\jre\\"))
 
 run / fork := true
-<<<<<<< HEAD
 mainClass in (Compile, packageBin) := Some("acrt.remoting.RemotingMain")
 
 mainClass in (Compile, run) := Some("acrt.remoting.RemotingMain")
-=======
-mainClass in (Compile, packageBin) := Some("acrt.raytracing.Main")
-
-mainClass in (Compile, run) := Some("acrt.raytracing.Main")
->>>>>>> 0412187e5f60c6e990c6666dc74cb2b5ce94de19
 
 libraryDependencies ++= Seq(
 	"com.typesafe.akka" %% "akka-actor" % "2.5.24",
--- src/main/scala/acrt/remoting/TikActor.scala
+++ src/main/scala/acrt/remoting/TikActor.scala
@@ -9,13 +9,15 @@ class TikActor extends Actor {
   private val nodes = "0 1 2 3 4 5 6 7 8".split(" ").map("131.194.71.13" + _.trim)
 
   val playerManagers = nodes.map { ip =>
+    println("make address" + ip)
       val address = Address("akka.tcp", "TestSystem", ip, 5150)
+    println("address " + ip)
       context.system.actorOf(Props(new TokActor).withDeploy(Deploy(scope = RemoteScope(address))), s"TokActor_$ip")
   }
   
   def receive: Actor.Receive = {
     case "start" => {
-      playerManagers.foreach(_ ! "tik")
+      playerManagers.foreach{pm => pm ! "tik"; println(s"sent tik to $pm")}
     }
     case "tok" => {
       Thread.sleep(5000)
