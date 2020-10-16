e114ac388430fdf61f74d700692be239c3dd8720
Kurt Hardee <khardee@trinity.edu>
2020-10-16 10:04:58 -0500

    messed with serialization bindings

--- src/main/scala/acrt/cluster/untyped/GeometryCreator.scala
+++ src/main/scala/acrt/cluster/untyped/GeometryCreator.scala
@@ -3,7 +3,7 @@ import swiftvis2.raytrace._
 import java.net.URL
 import data.CartAndRad
 
-sealed trait GeometryCreator extends Serializable {
+sealed trait GeometryCreator {
     def apply(num: String): Geometry
 }
 
